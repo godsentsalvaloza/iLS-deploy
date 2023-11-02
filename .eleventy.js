2b43c251e3e37ebbc153c12719146935fef43b83
Godsent Salvaloza <85384933+godsentsalvaloza@users.noreply.github.com>
2023-11-02 16:33:39 +0800

    prototype03

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/resources");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
   return {
     dir: {
